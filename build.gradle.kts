4bff3609a12faf8a142dcb4c9a8aa3623204169f
Angelo <angjelin003@gmail.com>
2026-09-05 01:32:51 +0200

    Migrate buildscript to plugins block
    Updated build script to use plugins block for dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-    }
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
